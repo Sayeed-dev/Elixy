d7184d15d82c64d124fc2aba330233530957e003
Sheikh Samaun Sayeed <samaunsayeed50@gmail.com>
2026-04-30 01:00:36 +0600

    Fixed some bugs

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
       <div className="text-gray-900 ">
         <div className="max-w-xl">
           <h1 className="mb-5 text-6xl font-bold">
-            Connect Freely. <br />Share Boldly. <br />Be You.
+             Freely. <br />Share Boldly. <br />Be You.
           </h1>
           <p className="mb-5">
             Join a space where your voice matters. Discover people who think
